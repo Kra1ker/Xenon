8f4fc6b5c7dcf48ec5b6a0d7ab6ed64f9b59be85
Tikimfox <tikim.fox@gmail.com>
2026-08-16 16:18:15 +0200

    Add ray cast code and item addition for mouse picking

--- scripts/world_drop.gd
+++ scripts/world_drop.gd
@@ -7,6 +7,7 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	return true
 	
 
+# Get items out (spawning instance in 2d world)
 func _drop_data(at_position: Vector2, data: Variant) -> void:
 	var node = WORLD_ITEM.instantiate()
 	
@@ -17,4 +18,30 @@ func _drop_data(at_position: Vector2, data: Variant) -> void:
 	data.item = null
 	node.global_position = Vector2(2, 1)
 	print("Droped out")
-	
+
+
+# Get items back
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			print("Pressed")
+			
+			var cam := get_viewport().get_camera_2d()
+			var space := cam.get_world_2d().direct_space_state
+			
+			var param = PhysicsRayQueryParameters2D.new()
+			param.from = cam.project_ray_origin(event.position)
+			param.to = param.from + cam.project_ray_normal(event.position) * 100
+			
+			var ray := space.intersect_ray(param)
+			if ray and ray["collider"] is RigidBody2D:
+				var world_item = ray["collider"]
+				for slot in %InventoryGridContainer.get_children():
+					if slot.item: continue
+					
+					# Gets first empty slot
+					slot.item = ray["collider"].get_meta("item_data")
+					slot.update_ui()
+					world_item.queue_free()
+					break
+					
