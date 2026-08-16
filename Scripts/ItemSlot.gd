9760b9953f21f05e308b8fc92d3728a27f5073ae
Tikimfox <tikim.fox@gmail.com>
2026-08-16 06:16:24 +0200

    Add base inventory slot script for duplication

--- Scripts/ItemSlot.gd
+++ Scripts/ItemSlot.gd
@@ -3,4 +3,10 @@ extends Panel
 @onready var icon: TextureRect = $Icon
 
 
-func _get_drag_data(at_position: Vector2) -> Variant:
+func _get_drag_data(_at_position: Vector2) -> Variant:
+	if icon.texture == null:
+		return
+	
+	var preview = duplicate()
+	set_drag_preview(preview)
+	return icon
