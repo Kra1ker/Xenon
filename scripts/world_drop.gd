73da5d2c84184bd4b1fb76d76d76b7fb3f4e6267
Tikimfox <tikim.fox@gmail.com>
2026-08-16 14:56:01 +0200

    Test: add advanced drag and drop system
    Base it on godot resources

---
+++ scripts/world_drop.gd
@@ -0,0 +1,19 @@
+extends Control
+
+const WORLD_ITEM = preload("uid://bba1rn5pflg44")
+
+
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	return true
+	
+
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	var node = WORLD_ITEM.instantiate()
+	
+	node.set_meta("iteam_data", data.item)
+	node.get_node("Sprite2D").icon = data.item.icon
+	
+	get_tree().current_scene.add_child(node)
+	data.item = null
+	node.global_position = Vector2(2, 1)
+	
