b989d4d805320236c21d86f07d2460fdd80b5c8d
Tikimfox <tikim.fox@gmail.com>
2026-08-16 06:24:52 +0200

    Fix: make item preview centered at the mouse cursor

--- Scripts/ItemSlot.gd
+++ Scripts/ItemSlot.gd
@@ -10,7 +10,7 @@ func _get_drag_data(_at_position: Vector2) -> Variant:
 	var preview = duplicate()
 	var c = Control.new()
 	c.add_child(preview)
-	preview.position -= Vector2(25, 25)
+	preview.position -= Vector2(75,75)
 	preview.self_modulate = Color.TRANSPARENT
 	c.modulate = Color(c.modulate, 0.5)
 	
