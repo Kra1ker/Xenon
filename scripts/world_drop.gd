1efbc66674344510b4704f9568ad85a3a1d41b9c
Tikimfox <tikim.fox@gmail.com>
2026-08-16 15:47:02 +0200

    Fix: items not droping out

--- scripts/world_drop.gd
+++ scripts/world_drop.gd
@@ -10,10 +10,11 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 func _drop_data(at_position: Vector2, data: Variant) -> void:
 	var node = WORLD_ITEM.instantiate()
 	
-	node.set_meta("iteam_data", data.item)
-	node.get_node("Sprite2D").icon = data.item.icon
+	node.set_meta("item_data", data.item)
+	node.get_node("Sprite2D").texture = data.item.icon  # Error
 	
 	get_tree().current_scene.add_child(node)
 	data.item = null
 	node.global_position = Vector2(2, 1)
+	print("Droped out")
 	
