98f1eff0a0c4a32a1ee76d52285b1d6cf5d87def
Tikimfox <tikim.fox@gmail.com>
2026-08-22 06:26:46 +0200

    Add item initialization
    (not working)

--- scripts/cauldron_minigame.gd
+++ scripts/cauldron_minigame.gd
@@ -2,6 +2,14 @@ extends Node2D
 
 var item_scene = preload("res://scenes/floating_banana.tscn")
 
+@export var item: ItemData :
+	set(value):
+		item = value
+# Test
+const ITEM_BANANA = preload("uid://bvsfyvn4yh0jy")  # Inventory branch item
+const ITEM_PENCIL = preload("uid://dt56lhnaqt2p2")  # Inventory branch item
+
+
 @export var items_count: int = 5
 @export var cauldron_radius: float = 200.0
 @onready var camera: Camera2D = $Camera2D
@@ -16,6 +24,10 @@ func _ready () -> void:
 func spawn_items() -> void:
 	# Spawn items using resources. Details: check world_drop.gd 
 	# (drop datafunction)
+	# Test: custom item spawning (those have inventory functionality)
+	spawn_an_item(ITEM_BANANA)
+	spawn_an_item(ITEM_PENCIL)
+
 	var center = get_viewport_rect().size / 2.0
 	
 	for i in range(items_count):
@@ -32,3 +44,15 @@ func spawn_items() -> void:
 
 func _on_item_caught(item_name: String) -> void:
 	print(item_name)
+
+func spawn_an_item(WORLD_ITEM) -> void:
+	var node = WORLD_ITEM.instantiate()  # Scene (lvl) to make items spawn in
+	
+	# SPAWNING items in the world (lvl)
+	node.get_node("Sprite2D").texture = item.icon
+	node.get_node("CollisionShape2D").shape = item.collision_shape
+	
+	get_tree().current_scene.add_child(node)
+	node.global_position = Vector2(randi_range(1000, 1000), 
+	randi_range(1000, 1000))
+	print("Item spawned")
