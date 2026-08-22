26230b44d9b3f5fceac529a5d4b7fd7d36976d1a
Tikimfox <tikim.fox@gmail.com>
2026-08-22 15:36:48 +0200

    Fix: item disappearing when changing items in inv

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -13,17 +13,11 @@ func _process(delta: float) -> void:
 
 var data_bak
 func _notification(what: int) -> void:
-	"""
-	NOTE: in case items don't render properly after changing places between
-	each other and occasionally disappear in the inventory (usually same 
-	items) reset this commit: "Refactor: inventory notification func"
-	<62f836e22f5950b7735324d53719ae428426034e>
-	"""
 	if what == Node.NOTIFICATION_DRAG_BEGIN:
 		on_drag_begin()
 	if what == Node.NOTIFICATION_DRAG_END:
-		on_drag_begin()
-
+		on_drag_end()
+	
 
 func on_drag_begin() -> void:
 	data_bak = get_viewport().gui_get_drag_data()
