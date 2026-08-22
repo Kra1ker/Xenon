075e542ee5ecb37a45e175f86386c473a99116fd
Tikimfox <tikim.fox@gmail.com>
2026-08-22 06:49:00 +0200

    Add items spawn on level instead
    not working

--- scripts/cauldron_minigame.gd
+++ scripts/cauldron_minigame.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 var item_scene = preload("res://scenes/floating_banana.tscn")
+var item_scene2 = preload("res://scenes/world_item.tscn")
+const BANANA = preload("uid://bvsfyvn4yh0jy")
+const PENCIL = preload("uid://dt56lhnaqt2p2")
 
+
+@export var item: ItemData
 @export var items_count: int = 5
 @export var cauldron_radius: float = 200.0
 @onready var camera: Camera2D = $Camera2D
@@ -16,6 +21,14 @@ func _ready () -> void:
 func spawn_items() -> void:
 	# Spawn items using resources. Details: check world_drop.gd 
 	# (drop datafunction)
+	var r = 800
+	spawn_an_item(BANANA, 
+	Vector2(randi_range(r, r), randi_range(r, r))
+	)
+	spawn_an_item(PENCIL,
+	Vector2(randi_range(r, r), randi_range(r, r))
+	)
+	
 	var center = get_viewport_rect().size / 2.0
 	
 	for i in range(items_count):
@@ -32,3 +45,9 @@ func spawn_items() -> void:
 
 func _on_item_caught(item_name: String) -> void:
 	print(item_name)
+
+func spawn_an_item(item_data: ItemData, position: Vector2) -> void:
+	var item = item_scene2.instantiate()
+	add_child(item)
+
+	item.global_position = position
