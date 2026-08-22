bae1e029d3b223c0758966b1378fa024a59987d8
Tikimfox <tikim.fox@gmail.com>
2026-08-22 14:59:50 +0200

    Refactor: inventory notification func
    NOTE: items rendering may be brocken, hard to reproduce

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -13,10 +13,23 @@ func _process(delta: float) -> void:
 
 var data_bak
 func _notification(what: int) -> void:
+	"""
+	NOTE: in case items don't render properly after changing places between
+	each other and occasionally disappear in the inventory (usually same 
+	items) reset this commit: "Refactor: inventory notification func"
+	<62f836e22f5950b7735324d53719ae428426034e>
+	"""
 	if what == Node.NOTIFICATION_DRAG_BEGIN:
-		data_bak = get_viewport().gui_get_drag_data()
+		on_drag_begin()
 	if what == Node.NOTIFICATION_DRAG_END:
-		if not is_drag_successful():
-			if data_bak:
-				data_bak.icon.show()
-				data_bak = null
+		on_drag_begin()
+
+
+func on_drag_begin() -> void:
+	data_bak = get_viewport().gui_get_drag_data()
+	
+
+func on_drag_end() -> void:
+	if not is_drag_successful() and data_bak:
+		data_bak.icon.show()
+		data_bak = null
