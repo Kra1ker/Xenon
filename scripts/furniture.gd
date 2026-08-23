9f068557fe5fb50a02b1cf4ab93cdcb77af5aed6
Tikimfox <tikim.fox@gmail.com>
2026-08-24 00:35:14 +0200

    Add scale sys for silhouette and ready varriants

--- scripts/furniture.gd
+++ scripts/furniture.gd
@@ -4,6 +4,9 @@ extends StaticBody2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var collision: CollisionPolygon2D = $CollisionPolygon2D
 
+const SCALE_READY := 0.9
+const SCALE_SILHOUETTE := 6.0
+
 @export var furniture: FurnitureData:
 	set(value):
 		furniture = value
@@ -16,9 +19,18 @@ func _ready() -> void:
 	update_furniture()
 
 
+func set_visual(ready: bool) -> void:
+	if ready:
+		sprite_2d.texture = furniture.texture_ready
+		sprite_2d.scale = Vector2.ONE * SCALE_READY
+	else:
+		sprite_2d.texture = furniture.silhouette_texture
+		sprite_2d.scale = Vector2.ONE * SCALE_SILHOUETTE
+	
+
 func update_furniture() -> void:
 	if not furniture:
 		return
 	
-	sprite_2d.texture = furniture.texture
-	collision.polygon = furniture.collision_shape.segments
+	set_visual(true)
+	#collision.polygon = furniture.collision_shape.segments
--- scripts/furniture_data.gd
+++ scripts/furniture_data.gd
@@ -3,6 +3,7 @@ extends Resource
 class_name FurnitureData
 
 @export var item_name: String
-@export var texture: Texture2D
+@export var texture_ready: Texture2D
+@export var texture_silhouette: Texture2D
 @export var collision_shape: Shape2D
 @export var craft: Array
