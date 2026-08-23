060c060b32538236ff6f6e879bf0d5f062b58409
Tikimfox <tikim.fox@gmail.com>
2026-08-23 22:57:43 +0200

    Add collision to showup of a furniture instance

--- scripts/furniture.gd
+++ scripts/furniture.gd
@@ -2,21 +2,23 @@
 extends StaticBody2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision: CollisionPolygon2D = $CollisionPolygon2D
 
 @export var furniture: FurnitureData:
 	set(value):
 		furniture = value
 		if is_node_ready():
-			update_visual()
+			update_furniture()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	update_visual()
+	update_furniture()
 
 
-func update_visual() -> void:
+func update_furniture() -> void:
 	if not furniture:
 		return
 	
 	sprite_2d.texture = furniture.texture
+	collision.polygon = furniture.collision_shape.segments
