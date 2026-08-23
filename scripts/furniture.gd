a5d4f2775afaa718c5af1467761cba8d09115a60
Tikimfox <tikim.fox@gmail.com>
2026-08-24 00:38:43 +0200

    Add code for showing .res in editor (furniture)

--- scripts/furniture.gd
+++ scripts/furniture.gd
@@ -13,24 +13,31 @@ const SCALE_SILHOUETTE := 6.0
 		if is_node_ready():
 			update_furniture()
 
+@export var preview_ready := true:
+	set(value):
+		preview_ready = value
+		if is_node_ready():
+			set_visual(value)
+			
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	update_furniture()
-
+	
 
 func set_visual(ready: bool) -> void:
 	if ready:
 		sprite_2d.texture = furniture.texture_ready
 		sprite_2d.scale = Vector2.ONE * SCALE_READY
+		collision.polygon = furniture.collision_ready
 	else:
-		sprite_2d.texture = furniture.silhouette_texture
+		sprite_2d.texture = furniture.texture_silhouette
 		sprite_2d.scale = Vector2.ONE * SCALE_SILHOUETTE
-	
+		collision.polygon = furniture.collision_silhouette
+		
 
 func update_furniture() -> void:
 	if not furniture:
 		return
+
+	set_visual(preview_ready)
 	
-	set_visual(true)
-	#collision.polygon = furniture.collision_shape.segments
--- scripts/furniture_data.gd
+++ scripts/furniture_data.gd
@@ -5,5 +5,8 @@ class_name FurnitureData
 @export var item_name: String
 @export var texture_ready: Texture2D
 @export var texture_silhouette: Texture2D
-@export var collision_shape: Shape2D
+
+@export var collision_ready: PackedVector2Array
+@export var collision_silhouette: PackedVector2Array
+
 @export var craft: Array
